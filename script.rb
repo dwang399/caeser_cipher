2d15494b0bcd0c25f8db6df360ffbd10a07d686e
Daniel <danielzwang97@gmail.com>
2022-08-20 00:18:19 -0700

    Add comments

--- script.rb
+++ script.rb
@@ -5,13 +5,16 @@ def caeser_cipher(string, number)
     shift = number % 26
     # Iterate through each value in the array
     string_array.map! do |num|
+      #Only shift if the letter was a-z or A-Z
       if num.between?(65, 90)
+        #Condition for ensuring Wrap from Z to A
         if (num + shift > 90) 
           num = (num + shift - 26)
         else
           num = num + shift
         end
       elsif num.between?(97, 122)
+        #Condition for ensuring Wrap from z to a
         if (num + shift > 122)
           num = (num + shift - 26)
         else
@@ -21,6 +24,8 @@ def caeser_cipher(string, number)
         num = num
       end
     end
+    #Turn the ASCII Values back to letters
     string_array.map! { |num| num.chr }
+    #Join together the array into a string
     cipher = string_array.join("")
   end
