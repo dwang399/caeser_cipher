aa8f491ea9669e831ab5f3ca4f87db60081c526e
Daniel <danielzwang97@gmail.com>
2022-08-20 00:00:57 -0700

    Add conditional to ensure non letters do not change

--- script.rb
+++ script.rb
@@ -1,6 +1,14 @@
 def caeser_cipher(string, number)
     string_array = string.bytes
+    shift = number % 26
+    string_array.map! do |num|
+      if (num.between?(65, 90) || num.between?(97,122))
+        num = num + shift
+      else 
+        num = num
+      end
+    end
     string_array.map! { |num| num.chr }
     puts string_array
-  end
+end
   
